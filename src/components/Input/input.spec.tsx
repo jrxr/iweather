f97bfee84e162c066980953387deca92859c60c5
agacy junior <juniormartinssom@gmail.com>
2023-07-25 11:01:02 -0300

    render debug

--- src/components/Input/input.spec.tsx
+++ src/components/Input/input.spec.tsx
@@ -1,9 +1,9 @@
 import { render } from "@testing-library/react-native";
-
-import { Input } from "./index";
+import { Input } from ".";
 
 describe("Component: Input", () => {
   it("should be render without activity indicator", () => {
-    render(<Input />);
+    const { debug } = render(<Input />);
+    debug();
   });
 });

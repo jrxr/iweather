d50394dfce744cc96cb66960aa788269c8c50b3b
agacy junior <juniormartinssom@gmail.com>
2023-07-26 14:52:45 -0300

    Wrapper

--- src/contexts/cityContext.spec.tsx
+++ src/contexts/cityContext.spec.tsx
@@ -1,9 +1,10 @@
 import { useCity } from "@hooks/useCity";
 
 import { renderHook } from "@testing-library/react-native";
+import { CityProvider } from "./CityContext";
 
 describe("Context: CityContext", () => {
   it("should be change selected city.", () => {
-    const { result } = renderHook(() => useCity());
+    const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
   });
 });

f7005bb1cba820d57745083b43f9e3d9e7205100
agacy junior <juniormartinssom@gmail.com>
2023-07-26 14:58:26 -0300

    Updating the state

--- src/contexts/cityContext.spec.tsx
+++ src/contexts/cityContext.spec.tsx
@@ -7,6 +7,17 @@ describe("Context: CityContext", () => {
   it("should be change selected city.", async () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
 
-    await waitFor(() => act(() => console.log(result)));
+    await waitFor(() =>
+      act(() =>
+        result.current.handleChanceCity({
+          id: "1",
+          name: "São Paulo",
+          latitude: 123,
+          longitude: 456,
+        })
+      )
+    );
+
+    expect(result.current.city?.name).toBe("São Paulo");
   });
 });

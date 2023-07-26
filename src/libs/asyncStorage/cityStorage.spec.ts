9e4b7869c60d72da760cc409efeda7a9d61c12e1
agacy junior <juniormartinssom@gmail.com>
2023-07-26 10:20:20 -0300

    Native module mockup

--- src/libs/asyncStorage/cityStorage.spec.ts
+++ src/libs/asyncStorage/cityStorage.spec.ts
@@ -1,7 +1,8 @@
 import { getStorageCity } from "./cityStorage";
 
 describe("Storage: CityStorage", () => {
-  it("should be return null when don't have a city storaged", async () => {
+  it("should be return null when dont have a city storaged", async () => {
     const response = await getStorageCity();
+    expect(response).toBeNull();
   });
 });

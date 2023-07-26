3a3aee3625b12cc3ab9ead401955c71f85628184
agacy junior <juniormartinssom@gmail.com>
2023-07-26 15:20:41 -0300

    Using findByText

--- src/routes/routes.spec.tsx
+++ src/routes/routes.spec.tsx
@@ -1,8 +1,11 @@
-import { render } from "@testing-library/react-native";
+import { render, screen, waitFor } from "@testing-library/react-native";
 import { Routes } from ".";
 
 describe("Routes", () => {
-  it("should be render Search screen when not city selected", () => {
+  it("should be render Search screen when not city selected", async () => {
     render(<Routes />)
+
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i));
+    expect(title).toBeTruthy();
   });
 });
